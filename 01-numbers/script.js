ddb3fbe41b0634aa0bae9287c302677abeb9b0f3
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-10-11 15:36:31 +0200

    correzione primo quesito con somma

--- 01-numbers/script.js
+++ 01-numbers/script.js
@@ -6,8 +6,11 @@ const numThree = 32; // NON MODIFICARE QUESTA RIGA
 // gli operatori numerici di Javascript
 
 // 1. Imposta questa variabile come somma di numOne e numTwo
-const numOnePlusNumTwo = 24;
-console.log(numOnePlusNumTwo); // Output: 24
+const numberA = 20;
+const numberB = 4;
+
+const somma = numberA + numberB ;
+console.log(somma); // Output: 24
 
 // 2. Imposta questa variabile come prodotto di numThree e numTwo
 const numThreeTimesNumTwo = NaN;
